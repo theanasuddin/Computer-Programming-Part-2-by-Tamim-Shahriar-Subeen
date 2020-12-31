eff1b528f38429b9129409fae274ffea9ce78058
Anas Uddin <anasuddin44@gmail.com>
2020-12-31 15:32:15 +0600

    Add files via upload

---
+++ program3_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  FILE *fp;
+  char filename[] = "my_file.txt";
+
+  fp = fopen(filename, "a+");
+
+  fprintf(fp, "This is a file created by my program! ");
+  fprintf(fp, "I am so happy.");
+
+  fclose(fp);
+  return 0;
+}
