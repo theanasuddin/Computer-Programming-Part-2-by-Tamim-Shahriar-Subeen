b929982580d3d48de09f98e98dc6efa89a6e587d
Anas Uddin <anasuddin44@gmail.com>
2020-12-31 15:34:02 +0600

    Update and rename program3_1.c to অধ্যায় ৩ - ফাইল (File)/program3_1.c

--- অধ্যায় ৩ - ফাইল (File)/program3_1.c
+++ অধ্যায় ৩ - ফাইল (File)/program3_1.c
@@ -5,7 +5,7 @@ int main()
   FILE *fp;
   char filename[] = "my_file.txt";
 
-  fp = fopen(filename, "a+");
+  fp = fopen(filename, "w");
 
   fprintf(fp, "This is a file created by my program! ");
   fprintf(fp, "I am so happy.");
